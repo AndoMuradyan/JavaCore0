e8111dcebeca6c5f243d3a12ca15e2045bae85a1
user <and.m97987@gmail.com>
2022-07-02 11:08:07 +0400

    homwork books

---
+++ src/homework/books/storage/AuthorStorage.java
@@ -0,0 +1,48 @@
+package homework.books.storage;
+
+import homework.books.book.Author;
+
+
+public class AuthorStorage {
+    private Author[] authors = new Author[10];
+    private int size = 0;
+
+
+    public void add(Author author) {
+        if (authors.length == size) {
+            extend();
+        }
+        authors[size++] = author;
+    }
+
+    private void extend() {
+        Author[] temp = new Author[authors.length + 10];
+        for (int i = 0; i < size; i++) {
+            temp[i] = authors[i];
+        }
+        authors = temp;
+    }
+
+
+    public void printAllAuthor() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(i + "." + authors[i]);
+
+        }
+        System.out.println();
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Author getAuthorByIndex(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+
+        return authors[index];
+    }
+
+
+}
